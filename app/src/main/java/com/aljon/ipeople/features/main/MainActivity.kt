131319dd39250f969faf8993ff0948012c8a8908
ajcordenete <aljon.cordenete@gmail.com>
2020-07-19 09:56:54 +0800

    added navigation graph for Main activity

--- app/src/main/java/com/aljon/ipeople/features/main/MainActivity.kt
+++ app/src/main/java/com/aljon/ipeople/features/main/MainActivity.kt
@@ -2,15 +2,10 @@ package com.aljon.ipeople.features.main
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.aljon.ipeople.R
 import com.aljon.ipeople.base.BaseViewModelActivity
 import com.aljon.ipeople.databinding.ActivityMainBinding
-import com.aljon.ipeople.features.auth.landing.LandingActivity
-import com.aljon.module.common.ninjaTap
-import io.reactivex.rxkotlin.subscribeBy
-import timber.log.Timber
 
 class MainActivity : BaseViewModelActivity<ActivityMainBinding, MainViewModel>() {
     companion object {
@@ -26,57 +21,7 @@ class MainActivity : BaseViewModelActivity<ActivityMainBinding, MainViewModel>()
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setListeners()
-        setUpViewModelsObserver()
-    }
-
-    private fun setListeners() {
-        binding
-            .logout
-            .ninjaTap {
-                viewModel.logoutUserSession()
-            }
-            .apply { disposables.add(this) }
-    }
-
-    private fun setUpViewModelsObserver() {
-        viewModel
-            .state
-            .observeOn(scheduler.ui())
-            .subscribeBy(
-                onNext = { state ->
-                    handleState(state)
-                },
-                onError = {
-                    Timber.e(it)
-                }
-            )
-            .apply { disposables.add(this) }
-
-        viewModel
-            .session
-            .observe(this, Observer { user ->
-                val text =
-                    getString(
-                        R.string.welcome_to_main_screen,
-                        user.name
-                    )
-
-                binding
-                    .userInfo
-                    .text = text
-            })
-    }
-
-    private fun handleState(state: MainState?) {
-        when (state) {
-            is MainState.LogoutSuccess -> {
-                LandingActivity.openActivity(this)
-                finishAffinity()
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
